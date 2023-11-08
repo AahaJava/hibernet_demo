9a48589463d2c0c585665341e62484e0b00f1d11
Aasif <asif.java88@gmail.com>
2023-11-08 10:36:13 +0530

    Changes

--- hibernet/src/main/java/com/orm/demo/MySQL_CRUD_Opr.java
+++ hibernet/src/main/java/com/orm/demo/MySQL_CRUD_Opr.java
@@ -1,11 +1,7 @@
 package com.orm.demo;
-import java.io.File;
-import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.List;
 
 import org.hibernate.Query;
-import org.hibernate.ScrollableResults;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
